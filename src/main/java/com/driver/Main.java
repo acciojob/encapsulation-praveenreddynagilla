467b8ac2ba1fdc97a129459bf15d779c88c76f4c
Praveen <praveenreddynagilla1996@gmail.com>
2023-05-16 07:10:03 +0530

    Solving Encapsulation Problem

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj=new RWOnly();
+//        obj.name="praveen";
+//        System.out.println(obj.name); //java: name has private access in com.driver.RWOnly
+        obj.setName("Developer");
+        System.out.println(obj.getName());
+    }
   
 }
